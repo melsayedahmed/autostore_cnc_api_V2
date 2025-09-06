ddbdebe99c88e2c21065f82c51334f5e15e8668e
Mohab Mohammed <109934638+melsayedahmed@users.noreply.github.com>
2025-09-06 14:40:13 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -70,7 +70,7 @@ export default function HomePageClient() {
           viewport={{ once: false, amount: 0.2 }}
           className="text-3xl font-bold mb-8 text-black dark:text-white"
         >
-          Browse Vehicle Parts
+          Browse Vehicle Parts.
         </motion.h1>
 
         {/* TypesList */}
